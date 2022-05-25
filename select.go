8a396ba1f83d1c93aeb9ad476f9313b52605f87a
Leon LiuYang <leon@zuru.tech>
2022-05-25 10:17:48 +0800

    support  simple select

---
+++ select.go
@@ -0,0 +1,22 @@
+package golinq
+
+// current golang version 1.18.2 does not support generic in
+// anonymous function, nor generic type parameter in method
+// so here just return interface{}, it will break the type chain
+func (src IEnumerator[K, V]) Select(selector func(K, V) any) IEnumerator[int, any] {
+	return IEnumerator[int, any]{
+		Enumerate: func() IMoveNext[int, any] {
+			index := 0
+			moveNext := src.Enumerate()
+			return func() (k int, v any, ok bool) {
+				key, value, ok := moveNext()
+				if ok {
+					k = index
+					v = selector(key, value)
+					index++
+				}
+				return
+			}
+		},
+	}
+}
--- test_common.go
+++ test_common.go
@@ -8,25 +8,8 @@ type GenericTest struct {
 	AnyList   []interface{}
 	MapStrInt map[string]int
 	MapIntStr map[int]string
+	MapIntInt map[int]int
 	RuneList  []rune
 	ChanFloat chan float64
 	FloatList []float64
-}
-
-func RebuildSlice[K comparable, V any](e IEnumerator[K, V]) []V {
-	res := make([]V, 0)
-	moveNext := e.Enumerate()
-	for _, v, ok := moveNext(); ok; _, v, ok = moveNext() {
-		res = append(res, v)
-	}
-	return res
-}
-
-func RebuildMap[K comparable, V any](e IEnumerator[K, V]) map[K]V {
-	res := make(map[K]V)
-	moveNext := e.Enumerate()
-	for k, v, ok := moveNext(); ok; k, v, ok = moveNext() {
-		res[k] = v
-	}
-	return res
 }
